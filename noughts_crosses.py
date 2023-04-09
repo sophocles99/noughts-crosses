5c227cbe747a78ea97efae300bfb8e6b74b79b0b
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-04-09 07:17:49 +0100

    Shortened variable names, tidied up function ordering

--- noughts_crosses.py
+++ noughts_crosses.py
@@ -1,47 +1,3 @@
-def get_winning_lines():
-    winning_lines = []
-    for i in range(0, board_len, board_size):
-        winning_lines.append([j for j in range(i, i + board_size)])
-    for i in range(0, board_size):
-        winning_lines.append([j for j in range(i, board_len, board_size)])
-    winning_lines.append([i for i in range(0, board_len, board_size + 1)])
-    winning_lines.append([i for i in range(board_size - 1, (board_len) - 1, board_size - 1)])
-    return (winning_lines)
-
-def move_is_win(_move, _symbol, board):
-    for line in winning_lines:
-        if _move - 1 in line:
-            if all((board[pos] == _symbol) for pos in line):
-                return True
-    return False
-
-def computer_move(board):
-    move_scores = {pos: 0 for pos in range(board_len) if board[pos] == ' '}
-    for line in winning_lines:
-        line_contents = [board[pos] for pos in line]
-        if line_contents.count('O') == board_size - 1 and line_contents.count(' ') == 1:
-            return line[line_contents.index(' ')] + 1
-        if line_contents.count('X') == board_size -1 and line_contents.count(' ') == 1:
-            return line[line_contents.index(' ')] + 1
-        if 'X' not in line_contents:
-            for i in range(len(line_contents)):
-                if line_contents[i] == ' ':
-                    move_scores[line[i]] += 1
-    return max(move_scores, key=move_scores.get) + 1
-
-def display_board(board, board_size, board_len, is_demo = False):
-    print('-' * (board_size * (5 if is_demo else 4) + 1))
-    for i in range(0, board_len, board_size):
-        row_string = '|'
-        for j in range(i, i + board_size):
-            if is_demo:
-                row_string += ' ' + str(board[j]).ljust(2)
-            else:
-                row_string += ' ' + str(board[j])
-            row_string += ' |'
-        print(row_string)
-        print('-' * (board_size * (5 if is_demo else 4) + 1))
-
 def get_integer(min, max, message):
     while True:
         try:
@@ -54,6 +10,51 @@ def get_integer(min, max, message):
         break
     return _int
 
+def display_board(board, b_size, is_demo = False):
+    max_width = len(str(b_size ** 2)) if is_demo else 1
+    print('-' * (b_size * (max_width + 3) + 1))
+    for i in range(0, b_size ** 2, b_size):
+        row_string = '|'
+        for j in range(i, i + b_size):
+            row_string += ' ' + str(board[j]).ljust(max_width)
+            row_string += ' |'
+        print(row_string)
+        print('-' * (b_size * (max_width + 3) + 1))
+
+def get_winning_lines():
+    win_lines = []
+    b_len = b_size ** 2
+    for i in range(0, b_len, b_size):
+        win_lines.append([j for j in range(i, i + b_size)])
+    for i in range(0, b_size):
+        win_lines.append([j for j in range(i, b_len, b_size)])
+    win_lines.append([i for i in range(0, b_len, b_size + 1)])
+    win_lines.append([i for i in range(b_size - 1, (b_len) - 1, b_size - 1)])
+    return (win_lines)
+
+def computer_move(board):
+    move_scores = {pos: 0 for pos in range(b_size ** 2) if board[pos] == ' '}
+    for line in win_lines:
+        line_contents = [board[pos] for pos in line]
+        if line_contents.count('O') == b_size - 1 and \
+           line_contents.count(' ') == 1:
+            return line[line_contents.index(' ')] + 1
+        if line_contents.count('X') == b_size -1 and \
+           line_contents.count(' ') == 1:
+            return line[line_contents.index(' ')] + 1
+        if 'X' not in line_contents:
+            for i in range(len(line_contents)):
+                if line_contents[i] == ' ':
+                    move_scores[line[i]] += 1
+    return max(move_scores, key=move_scores.get) + 1
+
+def move_is_win(_move, _symbol, board):
+    for line in win_lines:
+        if _move - 1 in line:
+            if all((board[pos] == _symbol) for pos in line):
+                return True
+    return False
+
 def game_intro():
     print('\nNoughts and Crosses')
     print('===================\n')
@@ -62,22 +63,22 @@ def game_intro():
     num_players = get_integer(1, 2, 'Please enter number of players: ')
     
     print('\nWhat size of board would you like to play on?')
-    board_size = get_integer(1,10, 'Please enter board size: ')
-    board_len = board_size ** 2
+    b_size = get_integer(1,10, 'Please enter board size: ')
     print('Board squares are numbered as follows:')
-    demo_board = [str(i + 1) for i in range(board_len)]
-    display_board(demo_board, board_size, board_len, True)
-    return (num_players, board_size, board_len)
+    demo_board = [str(i + 1) for i in range(b_size ** 2)]
+    display_board(demo_board, b_size, True)
+    return (num_players, b_size)
 
 def play_game():
-    board = [' ' for _ in range(board_len)]
+    b_len = b_size ** 2
+    board = [' ' for _ in range(b_len)]
     current_player = 1
     is_draw = True
 
     print('\nCurrent board:')
-    display_board(board, board_size, board_len)
+    display_board(board, b_size)
 
-    for i in range(board_len):
+    for i in range(b_len):
         if current_player == 2 and num_players == 1:
             move = computer_move(board)
             print(f'Computer has played in square {move}')
@@ -88,7 +89,7 @@ def play_game():
                 move_message = \
                     f'Please enter move for Player {current_player}: '
             while True:    
-                move = get_integer(1, board_len, move_message)
+                move = get_integer(1, b_len, move_message)
                 if board[move - 1] != ' ':
                     print('That square is already taken\n')
                     continue
@@ -97,7 +98,7 @@ def play_game():
         symbol = 'X' if current_player == 1 else 'O'
         board[move - 1] = symbol
         print('\nCurrent board:')
-        display_board(board, board_size, board_len)
+        display_board(board, b_size)
 
         if move_is_win(move, symbol, board):
             if num_players == 1 and current_player == 1:
@@ -117,10 +118,10 @@ def play_game():
 
 while True:
     
-    num_players, board_size, board_len = game_intro()
-    winning_lines = get_winning_lines()
+    num_players, b_size = game_intro()
+    win_lines = get_winning_lines()
     play_game()
 
-    PLAY_AGAIN = input('Would you like to play another game? (Y/N) ')
-    if PLAY_AGAIN.lower()[0] != 'y':
+    play_again = input('Would you like to play another game? (Y/N) ')
+    if play_again.lower()[0] != 'y':
         break
